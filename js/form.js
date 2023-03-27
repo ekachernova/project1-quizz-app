523927255d9546840dbcafbf6e836780e0652a49
Ekaterina Chernova <chernova.berlin@gmail.com>
2023-03-27 18:40:56 +0200

    finish add a new card layout task

---
+++ js/form.js
@@ -0,0 +1,34 @@
+//add submit action to the form
+
+let submitButton = document.querySelector('[data-js="button__form__submit"]');
+const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  //   console.log(event.target);
+  const formElements = event.target.elements;
+  //   console.log(formElements.your__question.value);
+  const newCardData = document.createElement("section");
+  newCardData.classList.add("card");
+  newCardData.innerHTML = `
+<div id="card__bookmark">
+<img
+  src="./assets/bookmark_transparent.png"
+  alt="non-active-bookmark"
+  id="bookmark_checkbox"
+  data-js="bookmark_checkbox"
+/>
+</div>
+<h2>${formElements.your__question.value}</h2>
+<button data-js="card__button">Show Answer</button>
+<!--hidden answer-->
+<h2 data-js="card__answer" hidden>This is the answer!</h2>
+<div class="card__tags__container">
+<a href="#">#html</a>
+<a href="#">#flexbox</a>
+<a href="#">#css</a>
+</div>
+
+`;
+  document.body.append(newCardData);
+});
